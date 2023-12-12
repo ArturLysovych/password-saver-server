566c1eca568dcb27f59f3d7477f9949e74bea7af
ArturLysovych <arturlisovic@gmail.com>
2023-12-12 10:36:27 +0200

    Change User schema

--- src/users/users.schema.ts
+++ src/users/users.schema.ts
@@ -3,9 +3,11 @@ import * as mongoose from 'mongoose';
 export const UserSchema = new mongoose.Schema({
   login: String,
   password: String,
+  email: String
 });
 
 export interface User extends mongoose.Document {
   login: string,
   password: string,
+  email: string
 }
